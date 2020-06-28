f59d1e6c7dde3004d912b9bb3ddedd0b580ac049
Jeroen Bos <jeroen@deepinsight.io>
2020-06-28 23:28:26 +0200

    Remove dummy code

--- js/index.js
+++ js/index.js
@@ -1,12 +1,5 @@
 import * as katex from './blatex/dist/blatex';
 
-console.log(katex);
-console.log(Object.keys(katex));
-var arg0 = "c";
-var result = katex.renderToString(arg0);
-
-console.log(JSON.stringify(result));
-
 if (typeof globalThis !== 'undefined') {
     globalThis.katex = katex;
 }
