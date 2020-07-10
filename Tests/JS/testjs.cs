d5a174684f32061c492d5ecb8d4b551ac409ad05
Jeroen Bos <jeroen@deepinsight.io>
2020-07-10 18:35:40 +0200

    Enable data-loc html attributes

--- Tests/JS/testjs.cs
+++ Tests/JS/testjs.cs
@@ -20,7 +20,7 @@ namespace BlaTeX.Tests
         {
             var imports = $"{Program.RootFolder}/wwwroot/js/blatex_wrapper.js".ToSingleton();
             var result = await new NodeJSRuntime(imports).InvokeAsync<string>("blatex_wrapper.default.renderToString", new[] { "c" }).AsTask();
-            Assert(result == "<span class=\"katex\"><span class=\"katex-mathml\"><math><semantics><mrow><mi>c</mi></mrow><annotation encoding=\"application/x-tex\">c</annotation></semantics></math></span><span class=\"katex-html\" aria-hidden=\"true\"><span class=\"base\"><span class=\"strut\" style=\"height:0.43056em;vertical-align:0em;\"></span><span class=\"mord mathit\">c</span></span></span></span>");
+            Assert(result == "<span class=\"katex\"><span class=\"katex-mathml\"><math><semantics><mrow><mi>c</mi></mrow><annotation encoding=\"application/x-tex\">c</annotation></semantics></math></span><span class=\"katex-html\" aria-hidden=\"true\"><span class=\"base\"><span class=\"strut\" style=\"height:0.43056em;vertical-align:0em;\"></span><span class=\"mord mathit\" data-loc=\"0,1\">c</span></span></span></span>");
         }
 
         [Fact]
