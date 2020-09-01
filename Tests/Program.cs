b17b05715d84e46adf7f86e2b2213f2a841be197
Jeroen Bos <jeroen@deepinsight.io>
2020-09-02 01:46:08 +0200

    Debug using Nito.AsyncEx.AsyncContext

--- Tests/Program.cs
+++ Tests/Program.cs
@@ -1,14 +1,22 @@
 using JBSnorro.Testing;
+using JBSnorro.Threading;
 using System;
 using System.Diagnostics;
 using System.IO;
+using NitoAsyncContext = Nito.AsyncEx.AsyncContext;
 
 namespace BlaTeX.Tests
 {
     public class Program
     {
         [DebuggerHidden]
-        public static void Main(string[] args) => TestExtensions.DefaultMainTestProjectImplementation(args);
+        public static void Main(string[] args)
+        {
+            using (new NitoAsyncContext().SynchronizationContext.AsTemporarySynchronizationContext())
+            {
+                TestExtensions.DefaultMainTestProjectImplementation(args);
+            }
+        }
 
         public static string RootFolder
         {
