659eedae49a17ae116c367a433bef586c8d474bc
Jeroen Bos <jeroen@deepinsight.io>
2021-07-26 15:20:23 +0200

    Add HelperToFindTestTypes

--- Tests/Program.cs
+++ Tests/Program.cs
@@ -1,6 +1,9 @@
+using JBSnorro.Diagnostics;
+using JBSnorro.Extensions;
 using JBSnorro.Testing;
 using JBSnorro.Threading;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -31,10 +34,10 @@ namespace BlaTeX.Tests
 			}
 			else
 			{
+				// HelperToFindTestTypes(args);
 				TestExtensions.DefaultMainTestProjectImplementation(args);
 			}
 		}
-
 		public static string RootFolder
 		{
 			get
@@ -47,6 +50,28 @@ namespace BlaTeX.Tests
 						throw new InvalidOperationException("Cannot find project root folder");
 				}
 				return dir.FullName;
+			}
+		}
+
+		internal static void HelperToFindTestTypes(IReadOnlyList<string> args)
+		{
+			Contract.Requires(args.Count != 0);
+
+			string filter = args[0];
+			var types = typeof(Program).Assembly
+									   .GetTypes()
+									   .Where(type => type.Namespace.Contains("BlaTeX"))
+									   .Where(type => !type.Name.StartsWith("<"))
+									   .Where(type => type.FullName.Contains(filter, StringComparison.OrdinalIgnoreCase))
+									   .ToList();
+
+			if (types.Count == 0)
+				Console.WriteLine($"No types in this assembly have speakable names matching '{filter}'.");
+			foreach (Type type in types.OrderBy(t => t.FullName.Substring(t.Namespace.Length + ".".Length)))
+			{
+				string name = type.FullName.Substring(type.Namespace.Length + ".".Length);
+				int testCount = TestExtensions.GetTestMethods(type).Count();
+				Console.WriteLine($"{name} has {testCount} tests");
 			}
 		}
 	}
