719a40d24bd24c4687152a81b249b077cf0ee997
Jeroen Bos <jeroen@deepinsight.io>
2021-07-26 11:43:28 +0200

    Extract function HtmlEqualityComparer.ComputeException

--- Tests/HtmlEqualityComparer.cs
+++ Tests/HtmlEqualityComparer.cs
@@ -1,3 +1,4 @@
+#nullable enable
 using AngleSharp.Diffing.Core;
 using AngleSharp.Dom;
 using Bunit;
@@ -13,8 +14,8 @@ namespace BlaTeX
 {
 	public class HtmlEqualityComparer : IDifferenceComparer<string, IDiff>
 	{
+		private const string defaultMessage = "Html unequal";
 		public static HtmlEqualityComparer Instance { get; } = new HtmlEqualityComparer();
-
 
 		[DebuggerHidden]
 		public static IEnumerable<IDiff> ComputeDifference(string x, string y, out (INodeList X, INodeList Y) nodes)
@@ -24,25 +25,34 @@ namespace BlaTeX
 			var diffs = nodes.X.CompareTo(nodes.Y);
 			return diffs;
 		}
-		/// <summary> Throws a detailed error message if the specified strings don't represent equivalent HTML. </summary>
+		/// <summary> Throws a detailed error if the specified strings don't represent equivalent HTML. </summary>
 		[DebuggerHidden]
-		public static void AssertEqual(string expected, string input, string message = "Html unequal")
+		public static void AssertEqual(string expected, string input, string message = defaultMessage)
 		{
-			var diffs = HtmlEqualityComparer.ComputeDifference(expected, input, out (INodeList Expected, INodeList Input) nodes);
+			HtmlEqualException? exception = ComputeException(expected, input, message);
+			if (exception != null)
+				throw exception;
+		}
+		/// <summary> Gets a detailed error if the specified strings don't represent equivalent HTML. </summary>
+		[DebuggerHidden]
+		internal static HtmlEqualException? ComputeException(string expected, string input, string message = defaultMessage)
+		{
+			var diffs = ComputeDifference(expected, input, out (INodeList Expected, INodeList Input) nodes);
 
 			if (diffs.Any())
-				throw new HtmlEqualException(diffs, nodes.Expected, nodes.Input, message);
+				return new HtmlEqualException(diffs, nodes.Expected, nodes.Input, message);
+
+			return null;
 		}
 
 
 
-
 		protected HtmlEqualityComparer() { }
-		
+
 
 		[DebuggerHidden]
 		IEnumerable<IDiff> IDifferenceComparer<string, IDiff>.ComputeDifference(string x, string y) => ComputeDifference(x, y);
-		
+
 		[DebuggerHidden]
 		public static IEnumerable<IDiff> ComputeDifference(string x, string y) => ComputeDifference(x, y, out var _);
 	}
