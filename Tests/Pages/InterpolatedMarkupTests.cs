49b7f38f016e634a51439b60c87a16149a8bd675
Jeroen Bos <jeroen@deepinsight.io>
2021-07-27 17:12:25 +0200

    Add some simple tests to InterpolatedMarkupTests

---
+++ Tests/Pages/InterpolatedMarkupTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlaTeX.JSInterop.KaTeX;
+using BlaTeX.JSInterop.KaTeX.Syntax;
+using Bunit;
+using Bunit.Extensions;
+using JBSnorro;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+using BlaTeX.Tests.Mocks;
+using BlaTeX.Pages;
+
+namespace BlaTeX.Tests.Pages
+{
+	public class InterpolatedMarkupTests
+	{
+		protected static TestContext CreateTestContext(Option<string> renderToStringReturnValue = default)
+		{
+			var ctx = new TestContext();
+			ctx.Services.AddDefaultTestContextServices(ctx, new BunitJSInterop());
+			ctx.Services.Add<IKaTeXRuntime>(new MockKaTeXRuntime().With(renderToStringReturnValue));
+
+			return ctx;
+		}
+		static readonly Func<string, IEnumerable<Range>> dummySelector = s => throw new NotImplementedException();
+
+		[Fact]
+		public async Task CreatingWithoutArgumentsThrows()
+		{
+			var cut = new InterpolatedMarkup();
+
+			await Assert.ThrowsAsync<ArgumentException>(nameof(InterpolatedMarkup.Markup), () => cut.SetParametersAsync(ParameterView.Empty));
+			await Assert.ThrowsAsync<ArgumentException>(() => cut.SetParametersAsync(new { Markup = "" }));
+			await Assert.ThrowsAsync<ArgumentException>(nameof(InterpolatedMarkup.Substitute), () => cut.SetParametersAsync(new { Markup = "", Selector = dummySelector }));
+		}
+		[Fact]
+		public async Task CreatingWithNullArgumentsThrows()
+		{
+			var cut = new InterpolatedMarkup();
+
+			await Assert.ThrowsAsync<ArgumentNullException>(nameof(InterpolatedMarkup.Markup), () => cut.SetParametersAsync(new { Markup = (string)null }));
+			await Assert.ThrowsAsync<ArgumentNullException>(nameof(InterpolatedMarkup.Selector), () => cut.SetParametersAsync(new { Markup = "", Selector = (Func<string, IEnumerable<Range>>)null }));
+			await Assert.ThrowsAsync<ArgumentNullException>(nameof(InterpolatedMarkup.Substitute), () => cut.SetParametersAsync(new { Markup = "", Selector = dummySelector, Substitute = (object)null }));
+		}
+		[Fact]
+		public async Task CreatingWithWrongTypeThrows()
+		{
+			var cut = new InterpolatedMarkup();
+			await Assert.ThrowsAsync<InvalidCastException>(() => cut.SetParametersAsync(new { Markup = "", Selector = dummySelector, Substitute = new object() }));
+		}
+	}
+}
