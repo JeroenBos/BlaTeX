5dabd801da3d380189effee3266f79563bdf5039
Jeroen Bos <15310631+JeroenBos@users.noreply.github.com>
2025-05-04 22:39:18 +0200

    Refactor

--- src/KaTeX/IChildComponentMarkupService.cs
+++ src/KaTeX/IChildComponentMarkupService.cs
@@ -1,8 +1,17 @@
 namespace BlaTeX;
 
+/// <summary>
+/// A service that identifies parts of markup and known by what to substitute them.
+/// </summary>
 public interface IChildComponentMarkupService
 {
+    /// <summary>
+    /// Gets ranges in the markup that are to be substituted by this service.
+    /// </summary>
     IEnumerable<Range> Select(string markup);
+    /// <summary>
+    /// Given a part of markup returns the renderer.
+    /// </summary>
     RenderFragment Substitute(string markupFragment);
-    IReadOnlyList<AnyParseNode> Select(IReadOnlyList<AnyParseNode> ast);
+    // IReadOnlyList<AnyParseNode> Select(IReadOnlyList<AnyParseNode> ast);
 }
